f7e918357b32af763d0dea72a0edf08f42748d67
Whoswatching89 <108240524+Whoswatching89@users.noreply.github.com>
2022-09-22 16:59:45 -0400

    uadded menue and driver function
    updated menu and such

--- AirlineDriver.java
+++ AirlineDriver.java
@@ -1,5 +1,7 @@
 package macawsProject;
 
+import java.util.Scanner;
+
 /**
  * AirlineDriver Class
  * 
@@ -11,9 +13,50 @@ package macawsProject;
  */
 public class AirlineDriver {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+	public static void printSeatMap() {
+    	//print a seat map of the airplane by flight number
+		//4.	We need to keep a seat map for each flight (probably a 2-D array??)
+		}
+	
+	public static void printFlightInfo() {
+    	//print flight information by flight number example: code(from above), date, time, route, pilot assigned
+		//The flight code (ex 12RPAM) actually keeps the date, time, and route so it is up to you if you still include these fields
+		}
+	
+	public static void printPilots() {
+    	//11.	We need an option for printing each Pilot’s schedule for the week 
+		//(which flights are they flying---date, time direction.
+    	}
+	
+	public static void printReservation() {
+    	// print who is sitting in a certian seat not a requriment but probaby a good idea
+	   	}
+	
+	public static void printCustomerByNum() {
+    	//12.	We need an option for printing all the reservations for a give Customer if given their customer number.
+	   	}
+	
+	public static void bookReservation() {
+    	//book a reservation the meat and potatos
+    	}
+	
+	public static void cancelReservation() {
+    	//cancel a reservation the meat and potatos
+    	}
+	public static void printGrossIncome() {
+    	//print the Grossincome of the flight by flight number
+    	}
+	public static void closeConnection() {
+    	//Close the connection
+    	}
+	public static void searchReservation() {
+    	//Close the connection
+    	}
+	public static void searchDeleted() {
+    	//search deleted flights by reservation number
+    	}
 
-    }
 
 }
+
+
--- Menu.java
+++ Menu.java
@@ -1,5 +1,11 @@
 package macawsProject;
 
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+
 /**
  * Menu Class
  * 
@@ -9,6 +15,62 @@ package macawsProject;
  * @version Sep 22, 2022
  *
  */
-public class Menu {
 
+
+	int choice = 0;
+	while(choice !=11) {
+		choice = menu();
+		if(choice == 1) {
+			AirlineDriver.printSeatMap();
+		} else if (choice ==2) {
+			AirlineDriver.printFlightInfo();
+		} else if (choice == 3) {
+			AirlineDriver.printPilots();
+		} else if (choice == 4) {
+			AirlineDriver.printReservation();
+		} else if (choice == 5) {
+			AirlineDriver.printCustomerByNum();
+		} else if (choice ==6) {
+			AirlineDriver.bookReservation();
+		} else if (choice ==7) {
+			AirlineDriver.cancelReservation();
+		} else if (choice ==8) {
+			AirlineDriver.printGrossIncome();
+		} else if (choice ==9) {
+			AirlineDriver.searchReservation();
+		} else if (choice ==10) {
+			AirlineDriver.searchDeleted();
+		} else if (choice ==11) { 
+			AirlineDriver.closeConnection();
+			System.exit(0);
+			System.out.println("Thank you for choosing Java Airlines!\nHave a good day :).");
+		}
+	}
+	
 }
+	
+
+
+public static int menu() {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("\nMenu:");
+	System.out.println("1.  Generate a map of the seats.");
+	System.out.println("2.  Generate flight information.");
+	System.out.println("3.  Generate Pilots.");
+	System.out.println("4.  Generate the Reservations by Flight and Seat Number.");
+	System.out.println("5.  Generate Customer information by Number.");
+	System.out.println("6.  Book a Reservation.");
+	System.out.println("7.  Cancel Reservation.");
+	System.out.println("8.  Generate Profit by flight number.");
+	System.out.println("9.  Search Reservations");
+	System.out.println("10. Search deleted flights by reservation number.");
+	System.out.println("11.  Exit.");
+	int ans = scan.nextInt();
+	return ans;
+	
+	}
+	
+	
+	
+}
+
